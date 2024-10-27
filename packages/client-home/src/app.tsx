114ce722bb64c6ae4bdd1c9f43983efb642975ae
Viktor Zubtsov <OUT-Zubtsov.V.A@sbertech.ru>
2024-10-27 19:41:49 +0300

    refactor(accountsbadge): refactor common

--- packages/client-home/src/app.tsx
+++ packages/client-home/src/app.tsx
@@ -1,46 +1,16 @@
-import { Button, Links, AccountsBadge, MainStyle } from "vite-common";
-import { plasma_b2c__dark} from '@salutejs/plasma-tokens/themes';
-import {createGlobalStyle} from 'styled-components';
-import {background, gradient, text} from '@salutejs/plasma-tokens';
-
-const test ={label: 'aaaaa', href: 'https://www.google.com'};
-const DocumentStyle = createGlobalStyle`
-    html {
-        color: ${text};
-        background-color: ${background};
-        background-image: ${gradient};
-    }
-`;
-
-const GlobalStyle = () => <DocumentStyle />;
-import {DeviceThemeProvider} from '@salutejs/plasma-ui';
-function currentTheme() {
-    return plasma_b2c__dark;
-}
-
-const ThemeStyle = createGlobalStyle(currentTheme());
+import { CoreProviders, AccountsBadge  } from "vite-common";
 
 function App() {
 
   return (
-    <>
-        <DeviceThemeProvider responsiveTypo>
-            <div>
-                <AccountsBadge
-                    text={"MOCK_ACCOUNT.name"}
-                    size="l"
-                    $isDone={false}
-                    $colorCode={"green"}
-                />
-                <Button label={'wdw'}/>
-                <Links links={[test, {label: 'wsdw', href: 'https://www.google.com'}]}/>
-            </div>
-            <GlobalStyle/>
-            <ThemeStyle/>
-        </DeviceThemeProvider>
-        <MainStyle/>
-
-    </>
+    <CoreProviders>
+        <AccountsBadge
+            text={"MOCK_ACCOUNT.name"}
+            size="l"
+            $isDone={false}
+            $colorCode={"green"}
+        />
+    </CoreProviders>
   )
 }
 
--- packages/vite-common/src/index.ts
+++ packages/vite-common/src/index.ts
@@ -1,3 +1,3 @@
 export * from "./components";
 export * from "./style/index.ts";
-export * from "./constant/index.ts";
+export * from "./providers";
