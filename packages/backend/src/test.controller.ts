3a55abc982e15a8325e76b043fc2e5b0cd0bdfd2
Viktor Zubtsov <58132336+ViktorZubtsov@users.noreply.github.com>
2024-10-28 13:51:35 +0300

    Update test.controller.ts

--- packages/backend/src/test.controller.ts
+++ packages/backend/src/test.controller.ts
@@ -1,1 +1,10 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('api/test')
+export class TestController {
+    @Get()
+    getTestData() {
+        return { message: 'Hello, this is your test data!' };
+    }
+}
 
